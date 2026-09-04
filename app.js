91bafc93c6f9c40a4a2b5f374d1761ce097e5a2f
Tanvi Gune <gunetanvi@gmail.com>
2026-09-04 12:50:51 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// Sample Todo Application Code
+function addTodoItem(task) {
+    if (!task) {
+        console.log("Task cannot be empty");
+        return;
+    }
+    let todoList = [];
+    todoList.push(task);
+    console.log("Added task: " + task);
+    return todoList;
+}
+
+addTodoItem("Learn DevOps with Jenkins and SonarQube");
